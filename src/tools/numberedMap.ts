73b110f4ed7e1a1aae3cb27e95540dc4ddaa58ee
Kevin Haube <khaube@fearless.tech>
2022-10-29 21:19:11 -0400

    Update jsdoc

--- src/tools/numberedMap.ts
+++ src/tools/numberedMap.ts
@@ -1,7 +1,16 @@
-import {NoteMap} from "../notes";
-
+/** Given an array, returns a numbered map starting at index 1
+ * Handy for nicely printed console outputs
+ *
+ * @example
+ * NumberedMap(["a", "b", "c"]);
+ * // Map(3) {
+ * //   1 => 'a',
+ * //   2 => 'b',
+ * //   3 => 'c',
+ * // }
+ * */
 export const NumberedMap = (arr: any[]) => {
-    const map: NoteMap = new Map<number, typeof arr[number]>()
+    const map = new Map<number, typeof arr[number]>()
     arr.forEach((val, index) => {
         map.set(index + 1, val);
     })
